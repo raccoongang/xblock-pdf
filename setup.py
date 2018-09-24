4613b2c609d1e2e93aee47f5ab35aa97d1e15ba3
Andrey Lykhoman <andrey.likhoman@gmail.com>
2018-09-24 15:10:56 +0300

    create base struct

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def package_data(pkg, root):
 
 setup(
     name='xblock-pdf',
-    version='v1.0.0-rg',
+    version='v1.1.0',
     description='Course component (Open edX XBlock) that provides an easy way to embed a PDF',
     packages=[
         'pdf',
@@ -29,5 +29,5 @@ setup(
             'pdf = pdf.pdf:PdfBlock',
         ]
     },
-    package_data=package_data('pdf', 'static'),
+    package_data=package_data('pdf', ['static', 'templates', 'translations']),
 )
